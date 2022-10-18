c646be2e83e2ae63dc91712d54f0cf523981303d
Wasiur Rahman <osiur2@gmail.com>
2022-10-18 20:01:42 +0600

    Jast Add Line

--- as.js
+++ as.js
@@ -1,26 +1,21 @@
 const fifth =()=> {
 }
-
 const fourth =()=> {
     fifth();
 }
-
 const third = () => { 
    const b = 5;
    fourth();
    const c = 10;
 }
-
 const second=(x)=> {
    third();
    const c = 10;
 }
-
 const first =(x)=> { 
    if (x < 10){
        second(x + 30);
-   }
-   
+   }  
 }
 
 first(5);
--- fibonacchi.js
+++ fibonacchi.js
@@ -1,6 +1,6 @@
 // 1, 1, 1, 2, 2, 3, 4, 5, 7, 9,  fibonacchi seriz
 // 1, 1, 2, 3, 5, 8, 13, 21
-
+// 1, 1, 1, 3, 5, 9, 17, 31, 57
 
 const fib = (n)=> {
     if(n == 1 || n == 2 || n == 3) {
@@ -13,4 +13,4 @@ const fib = (n)=> {
 
         return a + b + c;
 }
-console.log(fib(7));
+console.log(fib(9));
