9fc8691a2b54d068e9f39592dd80424fbd640838
https://github.com/Wasi752 <osiur2@gmail.com>
2022-10-20 20:50:33 +0600

    Just Practice

---
+++ javascript_callstack.js
@@ -0,0 +1,12 @@
+const x = 500;
+const arab = () => {
+    const abeZamzam = 100;
+    const rawza = 200;
+    const amarat = ()=>{
+        return x;
+    }
+    return abeZamzam + rawza;
+const aa = amarat;
+aa();
+}
+arab();
