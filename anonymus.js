bee7843b86ce6a2e03e7823cac56f9144bf3ca79
https://github.com/Wasi752 <osiur2@gmail.com>
2023-02-11 06:55:00 +0600

    prac-3

--- anonymus.js
+++ anonymus.js
@@ -1,15 +1,17 @@
 let c = 300;
 const p = 7;
+const k = ()=>5000;
 const f = () => {
-    const rahim = (b) => 100 + b;
+    const rahim = (b, g) => 100 + b + g();
     const alif = ()=> rahim;
     const w = ()=> alif;
     return w;
 }
-const f = () => ()=> ()=> (b)=> 100 + b;
+const fh = () => ()=> ()=> (b)=> 100 + b;
 const x = f();
-const z = x(100);
-const q = z(60);
+const z = x();
+const q = z();
 const y = f;
-console.log(q(40));
-console.log(q(20));
+//console.log(q(40, 30));
+console.log(q(20, k));
+//console.log(q(10));
