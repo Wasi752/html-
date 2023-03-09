d83565a37d9b51b818ef4531bc07e66b43478de0
https://github.com/Wasi752 <osiur2@gmail.com>
2023-03-09 23:43:57 +0600

    Try to callBack-6

--- CallBackHomeWork/callBack.js
+++ CallBackHomeWork/callBack.js
@@ -1,7 +1,7 @@
 
 const i = 0;
 let counter = 0;
-let student = 28000;
+let student = 2800;
 
 const result = (c) => {
     const bukhari = 80;
@@ -25,3 +25,34 @@ const registration = (b) => {
 
 console.log(rollNumber(registration));
 console.log(registration(rollNumber))
+const falcon = result(80 + 90 + 76 + 88 + 70);
+console.log(falcon);
+console.log(result(registration() + 500));
+
+const first = () => {
+    const x = 10;
+    const second = () => {
+        const third = () => {
+            return 6;
+        }
+        return 5;
+    }
+    const fourth = () => {
+        const six = () =>{
+            return 7;
+        }
+        return six;
+    }
+    const p = fourth();
+    const s = second;
+    const d = fourth;
+    const f = x + p();
+    const seven = ()=>{
+        return s() + f;
+    }
+    const t = seven;
+    return t;
+
+}
+const ababil = first();
+console.log(ababil());
